873f36eb44c0c301a31b9613ef20b2ac831a9e08
felipsbreno <brendo.filipe2050@gmail.com>
2023-09-17 07:55:19 -0300

    feat: add the routes in register from fastify

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,21 @@
 import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
 
+import { createTransactionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+
 const app = fastify();
 
 app.register(fastifyCors, {
   origin: "*",
 });
+
+app.register(createTransactionRoute);
+app.register(generateAiCompletionRoute);
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
 
 app.listen({ port: 3000 }).then(() => {
   console.log("Server is running");
